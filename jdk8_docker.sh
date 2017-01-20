040fe47d3915aea8358f3f60f9936c99f731d927
lanedia <diarmuidlane@gmail.com>
2017-01-20 16:38:32 +0000

    Updating with missing steps

--- jdk8_docker.sh
+++ jdk8_docker.sh
@@ -2,8 +2,11 @@
 yum update -y
 cd /root/Downloads/
 yum install -y git sudo which lynx
-mv jdk1.7.0_80/ /opt/java
+mv jdk1.8.0_121/ /opt/java
 cd /opt/java
+alternatives --install /usr/bin/java java /opt/java/bin/java 2
+yes 1|alternatives --config java 
+alternatives --install /usr/bin/jar jar /opt/java/bin/jar 2
 alternatives --install /usr/bin/javac javac /opt/java/bin/javac 2
 alternatives --set jar /opt/java/bin/jar
 alternatives --set javac /opt/java/bin/javac
